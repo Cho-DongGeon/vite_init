8ebc640a732dc31530935dac5003ef002022ab51
조동건 <o207@aidsoft.kr>
2025-06-19 17:58:31 +0900

    vite_init

---
+++ src/API/supabase.js
@@ -0,0 +1,19 @@
+// import { createClient } from '@supabase/supabase-js';
+
+// const supabaseUrl = process.env.VITE_SUPABASE_URL;
+// const supabaseKey = process.env.VITE_SUPABASE_KEY;
+
+// export const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+
+
+/* 
+
+해당 프로젝트가 supabase를 사용하고 있다면, 아래 node package를 설치해야 합니다.
+
+npm install @supabase/supabase-js
+
+사용하지 않는다면, 이 파일은 삭제해도 됩니다.
+
+*/
---
+++ src/API/url.js
@@ -0,0 +1,12 @@
+export default {
+    URL: 'https://localhost:7152',
+    // URL: '', // Dev
+    // URL: '', // Live
+    ROUTES: {
+        register: '/',
+    },
+    HEADERS: {
+        // AppID: 'a016878d-e2ff-4ea6-876b-195125445df5',
+        // AppKey: 'vKahY3N3K1wf1J7GSmZHMIkodEFBAlTuZ6bIBbUSCgjfTHKz1lix877HN9hUF0zO',
+    },
+};
